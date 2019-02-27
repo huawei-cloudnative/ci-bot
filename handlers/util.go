cb19dfe08f4f46f3ac907351a7de7d5cb8e66335
pavan187 <dasaradhipavan457@gmail.com>
2019-02-27 18:06:12 +0530

    Label handling in PR Describe section

--- handlers/util.go
+++ handlers/util.go
@@ -6,19 +6,22 @@ import (
 
 var (
 	// label
-	labelReg       = regexp.MustCompile("^/[Ll][Aa][Bb][Ee][Ll]")
-	labelCancelReg = regexp.MustCompile("^/[Rr][Ee][Mm][Oo][Vv][Ee]-[Ll][Aa][Bb][Ee][Ll]")
+	LabelReg       = regexp.MustCompile("^/[Ll][Aa][Bb][Ee][Ll]")
+	LabelCancelReg = regexp.MustCompile("^/[Rr][Ee][Mm][Oo][Vv][Ee]-[Ll][Aa][Bb][Ee][Ll]")
 
 	// test
-	okToTestReg = regexp.MustCompile("^/[Oo][Kk]-[Tt][Oo]-[Tt][Ee][Ss][Tt]")
-	retestReg   = regexp.MustCompile("^/[Rr][Ee][Tt][Ee][Ss][Tt]")
-	testReg     = regexp.MustCompile("^/[Tt][Ee][Ss][Tt]")
+	OkToTestReg = regexp.MustCompile("^/[Oo][Kk]-[Tt][Oo]-[Tt][Ee][Ss][Tt]")
+	RetestReg   = regexp.MustCompile("^/[Rr][Ee][Tt][Ee][Ss][Tt]")
+	TestReg     = regexp.MustCompile("^/[Tt][Ee][Ss][Tt]")
 
 	// review and approve
-	lgtmReg          = regexp.MustCompile("^/[Ll][Gg][Tt][Mm]")
-	lgtmCancelReg    = regexp.MustCompile("^/[Ll][Gg][Tt][Mm] [Cc][Aa][Nn][Cc][Ee][Ll]")
-	approveReg       = regexp.MustCompile("^/[Aa][Pp][Pp][Rr][Oo][Vv][Ee]")
-	approveCancelReg = regexp.MustCompile("^/[Aa][Pp][Pp][Rr][Oo][Vv][Ee] [Cc][Aa][Nn][Cc][Ee][Ll]")
+	LgtmReg          = regexp.MustCompile("^/[Ll][Gg][Tt][Mm]")
+	LgtmCancelReg    = regexp.MustCompile("^/[Ll][Gg][Tt][Mm] [Cc][Aa][Nn][Cc][Ee][Ll]")
+	ApproveReg       = regexp.MustCompile("^/[Aa][Pp][Pp][Rr][Oo][Vv][Ee]")
+	ApproveCancelReg = regexp.MustCompile("^/[Aa][Pp][Pp][Rr][Oo][Vv][Ee] [Cc][Aa][Nn][Cc][Ee][Ll]")
+
+	//assign/unassign
+	AssignOrUnassing			 = regexp.MustCompile("(?mi)^/(un)?assign(( @?[-\\w]+?)*)\\s*$")
 )
 
 const (
